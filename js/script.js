9b7ea205dd3c1d7a64aaf7e9d4d09f42f354f2a0
xAbeer <abeerayyad2000@gmail.com>
2021-11-17 18:47:31 +0300

    added details books of game of thrones and css code > Relates#6

--- js/script.js
+++ js/script.js
@@ -21,7 +21,51 @@ fetch("https://anapioficeandfire.com/api/books")
         authors.textContent=`Authors: ${book.authors}`
         divDetalis.appendChild(authors)
         card.appendChild(divDetalis)
+        let divPublisher=document.createElement('div')
+        divPublisher.setAttribute('class','details')
+        let publisher=document.createElement('p')
+        publisher.textContent=`Publisher: ${book.publisher}`
+        divPublisher.appendChild(publisher)
+        card.appendChild(divPublisher)
 
+        let divCountry=document.createElement('div')
+        divCountry.setAttribute('class','details')
+        let country=document.createElement('p')
+        country.textContent=`Country: ${book.country}`
+        divCountry.appendChild(country)
+        card.appendChild(divCountry)
+        let divMediaType=document.createElement('div')
+        divMediaType.setAttribute('class','details')
+        let mediaType=document.createElement('p')
+        mediaType.textContent=`MediaType: ${book.mediaType}`
+        divMediaType.appendChild(mediaType)
+        card.appendChild(divMediaType)
+        
+        let divReleased=document.createElement('div')
+        divReleased.setAttribute('class','details')
+        let released=document.createElement('p')
+        released.textContent=`Released: ${book.released}`
+        divReleased.appendChild(released)
+        card.appendChild(divReleased)
+        
+        let divNumber=document.createElement('div')
+        divNumber.setAttribute('class','details')
+        let numberOfPages=document.createElement('p')
+        numberOfPages.textContent=`Number Of Pages: ${book.numberOfPages}`
+        divNumber.appendChild(numberOfPages)
+        card.appendChild(divNumber)
+
+        let divs=document.createElement('div')
+        divs.setAttribute('class','nameHover')
+        let bookImg=document.createElement('img')
+        bookImg.setAttribute('id','bookImage')
+        bookImg.src='book.png'
+        divs.appendChild(bookImg)
+        
+        let names=document.createElement('h2')
+        names.textContent=book.name
+        divs.appendChild(names)
+        card.appendChild(divs)
        
     })
 })
