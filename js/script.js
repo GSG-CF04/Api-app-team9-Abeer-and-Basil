9864c5574c3b62d59259c33490455d4ccd6312de
xAbeer <abeerayyad2000@gmail.com>
2021-11-22 09:19:15 +0300

    adding readme file to game of throns Api Relates#2

--- js/script.js
+++ js/script.js
@@ -1,73 +1,17 @@
-let app = document.querySelector('#root')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Of Thrones Book</title>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-let logo = document.createElement("img");
-logo.src = "logo.png";
-app.appendChild(logo)
-let container = document.createElement("div");
-container.setAttribute("class", "container");
-
-app.appendChild(container);
-fetch("https://anapioficeandfire.com/api/books")
-.then(resp=>resp.json())
-.then((data)=>{
-    data.forEach(book => {
-        console.log(data)
-        let card =document.createElement('div')
-        card.setAttribute('class','card')
-        container.appendChild(card)
-        let divDetalis=document.createElement('div')
-        divDetalis.setAttribute('class','details')
-        let authors=document.createElement('p')
-        authors.textContent=`Authors: ${book.authors}`
-        divDetalis.appendChild(authors)
-        card.appendChild(divDetalis)
-        let divPublisher=document.createElement('div')
-        divPublisher.setAttribute('class','details')
-        let publisher=document.createElement('p')
-        publisher.textContent=`Publisher: ${book.publisher}`
-        divPublisher.appendChild(publisher)
-        card.appendChild(divPublisher)
-
-        let divCountry=document.createElement('div')
-        divCountry.setAttribute('class','details')
-        let country=document.createElement('p')
-        country.textContent=`Country: ${book.country}`
-        divCountry.appendChild(country)
-        card.appendChild(divCountry)
-        let divMediaType=document.createElement('div')
-        divMediaType.setAttribute('class','details')
-        let mediaType=document.createElement('p')
-        mediaType.textContent=`MediaType: ${book.mediaType}`
-        divMediaType.appendChild(mediaType)
-        card.appendChild(divMediaType)
-        
-        let divReleased=document.createElement('div')
-        divReleased.setAttribute('class','details')
-        let released=document.createElement('p')
-        released.textContent=`Released: ${book.released}`
-        divReleased.appendChild(released)
-        card.appendChild(divReleased)
-        
-        let divNumber=document.createElement('div')
-        divNumber.setAttribute('class','details')
-        let numberOfPages=document.createElement('p')
-        numberOfPages.textContent=`Number Of Pages: ${book.numberOfPages}`
-        divNumber.appendChild(numberOfPages)
-        card.appendChild(divNumber)
-
-        let divs=document.createElement('div')
-        divs.setAttribute('class','nameHover')
-        let bookImg=document.createElement('img')
-        bookImg.setAttribute('id','bookImage')
-        bookImg.src='book.png'
-        divs.appendChild(bookImg)
-        
-        let names=document.createElement('h2')
-        names.textContent=book.name
-        divs.appendChild(names)
-        card.appendChild(divs)
-       
-    })
-})
-.catch(error=> console.log(error))
-
+    <link href="css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div id="root"></div>
+    <script src="js/script.js"></script>   
+</body>
+</html>
